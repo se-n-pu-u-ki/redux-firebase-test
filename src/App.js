10e45a5f5a39c46ed056de7f9e45fc1460093c60
se-n-pu-u-ki <1023.suzuki.m@gmail.com>
2020-02-20 19:45:18 +0900

    day`

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Mario Plan</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
